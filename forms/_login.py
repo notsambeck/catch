22bae5d1866cbb53d5503cb5efe45445b115fc97
thesamuelbeck@gmail.com <thesamuelbeck@gmail.com>
2018-01-29 03:54:21 +0000

    Saved automatically

--- forms/_login.py
+++ forms/_login.py
@@ -10,14 +10,17 @@ class _login (_loginTemplate):
     # You must call self.init_components() before doing anything else in this function
     self.init_components(**properties)
     
-    anvil.server.call('delete_cookie')
+    # anvil.server.call('delete_cookie')
     # anvil.server.reset_session()
     
+
+  def form_show (self, **event_args):
+    # This method is called when the HTML panel is shown on the screen
     me = anvil.users.get_user(allow_remembered=True)
     if me:
       print('already logged in')
       open_form('_play')
-      gibberish
     else:
       print('not logged in')
       self.content_panel.add_component(Login())
+
