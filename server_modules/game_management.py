d711a5934a1e454b0316aafe74305ce78216cb9a
thesamuelbeck@gmail.com <thesamuelbeck@gmail.com>
2018-02-02 22:30:40 +0000

    Saved automatically

--- server_modules/game_management.py
+++ server_modules/game_management.py
@@ -57,9 +57,9 @@ def get_games(wall_throws=0, server=False, quick=False):
             order.append(_id)
           games[_id] = game
   
-  order += waiting
+        order += waiting
+ 
   msg = msg.format(len(order))
-  
   anvil.server.session['order'] = order   # [_id]
   
   # internal use to generate game list for new user
